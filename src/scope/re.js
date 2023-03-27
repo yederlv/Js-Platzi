6d1d5a38177934afd718e125f8f26bd679ec2693
Yeder <ylvicente@keos.co>
2023-03-26 20:52:47 -0500

    [ADD] clase 04

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firtsName;
+firtsName = 'Jedi';
+console.log(firtsName);
+
+
+var lastName = 'David'; // declarar - asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+
+var seconName = 'David'; // declarar - asignar
+var seconName = 'Ana'; // reasignar
+console.log(seconName);
+
+
+let fruit = 'Apple'; // declarar - asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+// let fruit = 'Banana'; //No se puede
+
+
+const animal = 'Dog'; // declarar - asignars
+// animal =  'cat'; // reasginar
+console.log(animal);
+
+
+const vehicle = [];
+vehicle.push('🚓');
+console.log(vehicle);
+
+vehicle.pop();
+console.log(vehicle)
