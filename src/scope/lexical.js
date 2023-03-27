fb9fdb6ebde502aedf37109ccf1838efd37a79cd
Yeder <ylvicente@keos.co>
2023-03-26 21:07:57 -0500

    [ADD] clase 06

---
+++ src/scope/lexical.js
@@ -0,0 +1,22 @@
+const myGlobal = 0;
+
+function myFunction() {
+    const myNumber = 1;
+    console.log(myGlobal);
+
+    // Función Interna
+    function parent() {
+        const inner = 2;
+        console.log(myNumber, myGlobal);
+
+        function child() {
+            console.log(inner, myNumber, myGlobal);
+        }
+
+        return child();
+    }
+
+    return parent();
+}
+
+myFunction();
